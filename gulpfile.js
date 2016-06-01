8733fe904b15fcd905d9de6c70b07f3322003005
llan <15814501057@163.com>
2016-06-01 17:22:41 +0800

    创建amwares项目

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
+
+
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+
+};
+
+
+
+gulp.task('runKeystone', shell.task('node keystone.js'));
+
